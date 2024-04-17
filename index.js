35ef1f749e2e5b8d668a710c40a7371900509e9c
Pramod Mahto <Mahtop324@gmail.com>
2024-04-17 19:38:37 +0545

    changed to index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port =3000;
 
 app.get("/", (req, res) => {
   res.json({ Name: "Pramod" });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log("App is listing port 3000");
 });
