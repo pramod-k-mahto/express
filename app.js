b232f9ba39c889adca0bc4ad3ef357b419f243d2
Pramod Mahto <Mahtop324@gmail.com>
2024-04-17 19:22:24 +0545

    add vercel

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const express=require('express')
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
-const app=express()
+const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.json({ Name: "Pramod" });
+});
 
-app.get('/',(req,res)=>{
-
-    res.json({Name:"Pramod"})
-})
-
-
-app.listen(port,()=>{
-    console.log("App is listing port 3000")
-})
+app.listen(port, () => {
+  console.log("App is listing port 3000");
+});
